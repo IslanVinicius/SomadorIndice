7552695177dfd91a5997fa9148df88e34a70a93a
Islan Vinicius <islanvinicius007@gmail.com>
2024-09-05 20:14:05 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int indice = 12, soma = 0, k = 1;
+
+        while (k < indice){
+            k = k + 1;
+            soma = soma + k;
+        }
+        System.out.println(soma);
+    }
+}
